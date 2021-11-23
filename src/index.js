29bb0fb32330e4b294bbc9afdccfad9bbb2821ce
Evnomad <b.boy.bip@gmail.com>
2021-11-23 20:24:57 +0300

    init

---
+++ src/index.js
@@ -0,0 +1,15 @@
+'strict';
+import 'bootstrap';
+
+import './scss/main.scss';
+import './js/assets';
+
+// import 'lazyload';
+// import 'slick-carousel'
+// import 'smartwizard';
+// import 'jquery-validation';
+
+// (function () {
+//     $(function () {
+//     });
+// })();
---
+++ src/js/assets.js
@@ -0,0 +1,34 @@
+import '../images/zipkode.svg';
+import '../images/zipkode-black.svg';
+import '../images/hero.jpg';
+import '../images/amazon.png';
+import '../images/american-eagle.png';
+import '../images/apple.png';
+import '../images/dickies.png';
+import '../images/disney.png';
+import '../images/target.png';
+import '../images/how-it-works-step1.svg';
+import '../images/how-it-works-step2.svg';
+import '../images/how-it-works-step3.svg';
+import '../images/how-it-works-step4.svg';
+import '../images/liberty-statue.svg';
+import '../images/usbank-black.png';
+import '../images/usbank-grey.png';
+import '../images/usbank-violet.png';
+
+import '../images/trolley.svg';
+import '../images/scissors.svg';
+import '../images/location-pin.svg';
+import '../images/lightening.svg';
+import '../images/parcel.svg';
+import '../images/delivery-van.svg';
+
+import '../images/faq-bg.jpg';
+import '../images/noisy-pattern.jpg';
+
+import '../images/robot-icon.png';
+import '../images/mail-icon.png';
+import '../images/twitter-icon.png';
+
+import '../images/door.jpg';
+
