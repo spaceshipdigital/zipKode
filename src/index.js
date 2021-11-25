3265458f605182145123f8cb75939373c7dd6fe8
Evnomad <b.boy.bip@gmail.com>
2021-11-25 13:37:30 +0300

    animate logos every 3 seconds

--- src/index.js
+++ src/index.js
@@ -13,3 +13,39 @@ import './js/assets';
 //     $(function () {
 //     });
 // })();
+
+window.addEventListener('DOMContentLoaded', (event) => {
+    animateLogos();
+});
+
+function animateLogos() {
+    const heroLogos = document.querySelectorAll('.hero-logos');
+    const delay = 500;
+    let i = 0
+
+    if (heroLogos.length) {
+        const items = heroLogos[0].querySelectorAll('li');
+
+        activateCurrentElement(items);
+        setInterval(() => {
+            // reset logos
+            items.forEach(item => item.querySelector('.logo-3d').classList.remove('active'));
+            activateCurrentElement(items);
+        }, delay);
+    }
+
+    function activateCurrentElement(items) {
+        let indx = i % items.length;
+
+        // animate only visible
+        const style = window.getComputedStyle(items[indx]);
+        if ((style.display === 'none') || (style.visibility === 'hidden')) {
+            i++;
+            indx = i % items.length;
+        }
+
+        // rotate element
+        items[indx].querySelector('.logo-3d').classList.add('active');
+        i++;
+    }
+}
